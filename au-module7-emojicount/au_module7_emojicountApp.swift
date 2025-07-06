f662d22a620958e9c544f060e51986dbe19ecd0d
rockymtnpy <119886264+rockymtnpy@users.noreply.github.com>
2025-07-05 23:09:13 -0500

    Add files via upload
    changed padding to view correctly in landscape

---
+++ au-module7-emojicount/au_module7_emojicountApp.swift
@@ -0,0 +1,17 @@
+//
+//  au_module7_emojicountApp.swift
+//  au-module7-emojicount
+//
+//  Created by user280687 on 7/5/25.
+//
+
+import SwiftUI
+
+@main
+struct au_module7_emojicountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
